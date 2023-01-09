4f173bb1cf0c03d18d1931b8aeb9fac983ea5b79
Iuri Néviton <iurineviton@gmail.com>
2023-01-09 18:13:32 -0300

    fix assembler

--- src/main/java/com/nevitoniuri/ticketbusapi/resource/assembler/Assembler.java
+++ src/main/java/com/nevitoniuri/ticketbusapi/resource/assembler/Assembler.java
@@ -1,5 +1,11 @@
 package com.nevitoniuri.ticketbusapi.resource.assembler;
 
+/**
+ *
+ * @param <R> Response
+ * @param <I> input
+ * @param <E> Entity
+ */
 public interface Assembler<E, I, R> {
 
     E toEntity(I request);
--- src/main/java/com/nevitoniuri/ticketbusapi/resource/assembler/BilheteAssembler.java
+++ src/main/java/com/nevitoniuri/ticketbusapi/resource/assembler/BilheteAssembler.java
@@ -9,26 +9,41 @@ import com.nevitoniuri.ticketbusapi.domain.model.Passageiro;
 import com.nevitoniuri.ticketbusapi.domain.model.Poltrona;
 import com.nevitoniuri.ticketbusapi.resource.request.BilheteCreate;
 import com.nevitoniuri.ticketbusapi.resource.response.BilheteResponse;
+import com.nevitoniuri.ticketbusapi.service.BilheteService;
+import com.nevitoniuri.ticketbusapi.service.LinhaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Component
+@RequiredArgsConstructor
 public class BilheteAssembler implements Assembler<Bilhete, BilheteCreate, BilheteResponse> {
 
+    private final LinhaService linhaService;
 
     @Override
     public Bilhete toEntity(BilheteCreate request) {
+
+        var linha = linhaService.findById(Long.parseLong(request.linhaId()));
+
         return Bilhete.builder()
                 .codigo(request.codigo())
-                .linha(Linha.builder().id(Long.parseLong(request.linhaId())).build())
+                .linha(linha)
                 .status(StatusBilhete.EMITIDO)
                 .passageiro(Passageiro.builder().id(Long.parseLong(request.passageiroId())).build())
                 .poltrona(Poltrona.builder().id(Long.parseLong(request.poltronaId())).build())
                 .dataHoraViagem(DateUtil.handleDataHora(request.dataHoraViagem()))
                 .taxaEmbarque(Constantes.TAXA_EMBARQUE)
-                .desconto(null)
-                .valorTotal(null)
+                .valorTotal(calcularValorTotal(linha.getValor(), Constantes.TAXA_EMBARQUE,
+                        BigDecimal.valueOf(0)))
                 .dataHoraCompra(LocalDateTime.now())
                 .build();
+    }
+
+    public BigDecimal calcularValorTotal(BigDecimal valorLinha, BigDecimal taxaEmbarque, BigDecimal desconto) {
+        return valorLinha.add(taxaEmbarque).subtract(desconto);
     }
 
     @Override
